d188ebc2cf3746e85ac74d6bd2a42857ab9f7c1c
Carson Holloway <carsonalh@gmail.com>
2025-02-02 21:00:10 +1000

    Render some variable-width text and implement a seeking left/right cursor

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{alloc, mem, ptr};
 
 use file_tree::{FileTree, FileTreeItemType};
+use lipsum::lipsum;
 use windows::{
     core::*,
     Win32::{
@@ -11,6 +12,7 @@ use windows::{
             Gdi::{InvalidateRect, ValidateRect},
         },
         System::LibraryLoader::*,
+        UI::Input::KeyboardAndMouse::*,
         UI::WindowsAndMessaging::*,
     },
 };
@@ -18,199 +20,241 @@ use Common::{D2D1_COLOR_F, D2D_RECT_F, D2D_SIZE_U};
 
 pub mod file_tree;
 
+trait AsUtf16 {
+    fn as_utf16<'a>(&self, buffer: &'a mut Vec<u16>) -> &'a [u16];
+}
+
+impl AsUtf16 for String {
+    fn as_utf16<'a>(&self, buffer: &'a mut Vec<u16>) -> &'a [u16] {
+        buffer.clear();
+        for c in self.as_bytes() {
+            buffer.push(*c as u16);
+        }
+
+        return buffer.as_slice();
+    }
+}
+
 struct Window {
     hwnd: HWND,
+    direct_2d_factory: ID2D1Factory,
     render_target: ID2D1HwndRenderTarget,
     text_brush: ID2D1SolidColorBrush,
     hover_brush: ID2D1SolidColorBrush,
+    direct_write_factory: IDWriteFactory,
     text_format_regular: IDWriteTextFormat,
     text_format_bold: IDWriteTextFormat,
+    text_layout: IDWriteTextLayout,
     width: u32,
     height: u32,
-    file_tree: file_tree::FileTree,
+    contents: String,
+    cursor_pos: u32,
+    contents_utf16: Vec<u16>,
     mouse_position: Option<(u32, u32)>,
 }
 
 impl Window {
-    fn new(name: PCWSTR) -> Result<Box<Window>> {
-        unsafe {
-            let class_name = w!("RustWindowClass");
-            let h_instance: HINSTANCE = GetModuleHandleW(None)?.into();
-            let wnd_class = WNDCLASSW {
-                hInstance: h_instance,
-                lpszClassName: class_name,
-                lpfnWndProc: Some(Self::window_proc),
-                style: CS_HREDRAW | CS_VREDRAW,
+    unsafe fn new(name: PCWSTR) -> Result<Box<Window>> {
+        let class_name = w!("RustWindowClass");
+        let h_instance: HINSTANCE = GetModuleHandleW(None)?.into();
+        let wnd_class = WNDCLASSW {
+            hInstance: h_instance,
+            lpszClassName: class_name,
+            lpfnWndProc: Some(Self::window_proc),
+            style: CS_HREDRAW | CS_VREDRAW,
+            ..Default::default()
+        };
+        RegisterClassW(&wnd_class);
+
+        let window_ptr = alloc::alloc(alloc::Layout::new::<Self>()) as *mut Window;
+
+        let hwnd = CreateWindowExW(
+            WINDOW_EX_STYLE::default(),
+            class_name,
+            name,
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            None,
+            None,
+            Some(h_instance),
+            Some(window_ptr as *const core::ffi::c_void),
+        )?;
+
+        let direct_2d_factory: ID2D1Factory =
+            D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, None)?;
+        let render_target = direct_2d_factory.CreateHwndRenderTarget(
+            &D2D1_RENDER_TARGET_PROPERTIES::default(),
+            &D2D1_HWND_RENDER_TARGET_PROPERTIES {
+                hwnd: hwnd,
                 ..Default::default()
-            };
-            RegisterClassW(&wnd_class);
+            },
+        )?;
 
-            let window_ptr = alloc::alloc(alloc::Layout::new::<Self>()) as *mut Window;
+        let text_brush = render_target.CreateSolidColorBrush(
+            &D2D1_COLOR_F {
+                r: 1.0,
+                g: 1.0,
+                b: 1.0,
+                a: 1.0,
+            },
+            None,
+        )?;
 
-            let hwnd = CreateWindowExW(
-                WINDOW_EX_STYLE::default(),
-                class_name,
-                name,
-                WS_OVERLAPPEDWINDOW,
-                CW_USEDEFAULT,
-                CW_USEDEFAULT,
-                CW_USEDEFAULT,
-                CW_USEDEFAULT,
-                None,
-                None,
-                Some(h_instance),
-                Some(window_ptr as *const core::ffi::c_void),
-            )?;
+        let hover_brush = render_target.CreateSolidColorBrush(
+            &D2D1_COLOR_F {
+                r: 0.2,
+                g: 0.2,
+                b: 0.2,
+                a: 1.0,
+            },
+            None,
+        )?;
 
-            let factory: ID2D1Factory = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, None)?;
-            let render_target = factory.CreateHwndRenderTarget(
-                &D2D1_RENDER_TARGET_PROPERTIES::default(),
-                &D2D1_HWND_RENDER_TARGET_PROPERTIES {
-                    hwnd: hwnd,
-                    ..Default::default()
-                },
-            )?;
+        let direct_write_factory =
+            DWriteCreateFactory::<IDWriteFactory>(DWRITE_FACTORY_TYPE_ISOLATED)?;
 
-            let text_brush = render_target.CreateSolidColorBrush(
-                &D2D1_COLOR_F {
-                    r: 1.0,
-                    g: 1.0,
-                    b: 1.0,
-                    a: 1.0,
-                },
-                None,
-            )?;
+        let text_format_regular = direct_write_factory.CreateTextFormat(
+            w!("C:\\Windows\\Fonts\\segoeui.ttf"),
+            None,
+            DWRITE_FONT_WEIGHT_NORMAL,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            24f32,
+            w!("en-au"),
+        )?;
 
-            let hover_brush = render_target.CreateSolidColorBrush(
-                &D2D1_COLOR_F {
-                    r: 0.2,
-                    g: 0.2,
-                    b: 0.2,
-                    a: 1.0,
-                },
-                None,
-)?;
+        let text_format_bold = direct_write_factory.CreateTextFormat(
+            w!("C:\\Windows\\Fonts\\segoeui.ttf"),
+            None,
+            DWRITE_FONT_WEIGHT_BOLD,
+            DWRITE_FONT_STYLE_NORMAL,
+            DWRITE_FONT_STRETCH_NORMAL,
+            32f32,
+            w!("en-au"),
+        )?;
 
-            let direct_write_factory =
-                DWriteCreateFactory::<IDWriteFactory>(DWRITE_FACTORY_TYPE_ISOLATED)?;
+        let mut contents_utf16 = Vec::new();
+        let contents_str = String::from(lipsum(200));
 
-            let text_format_regular = direct_write_factory.CreateTextFormat(
-                w!("C:\\Windows\\Fonts\\segoeui.ttf"),
-                None,
-                DWRITE_FONT_WEIGHT_NORMAL,
-                DWRITE_FONT_STYLE_NORMAL,
-                DWRITE_FONT_STRETCH_NORMAL,
-                32f32,
-                w!("en-au"),
-            )?;
+        let contents = contents_str.as_utf16(&mut contents_utf16);
 
-            let text_format_bold = direct_write_factory.CreateTextFormat(
-                w!("C:\\Windows\\Fonts\\segoeui.ttf"),
-                None,
-                DWRITE_FONT_WEIGHT_BOLD,
-                DWRITE_FONT_STYLE_NORMAL,
-                DWRITE_FONT_STRETCH_NORMAL,
-                32f32,
-                w!("en-au"),
-            )?;
+        let mut window_rect: RECT = Default::default();
+        GetClientRect(hwnd, &mut window_rect)?;
 
-            let mut client_rect = RECT::default();
-            GetClientRect(hwnd, &mut client_rect)?;
+        let window_width = (window_rect.right - window_rect.left) as f32;
+        let window_height = (window_rect.bottom - window_rect.top) as f32;
 
-            ptr::write(
-                window_ptr,
-                Window {
-                    hwnd,
-                    render_target,
-                    text_brush,
-                    hover_brush,
-                    width: (client_rect.right - client_rect.left) as u32,
-                    height: (client_rect.bottom - client_rect.top) as u32,
-                    file_tree: FileTree::new_in_working_dir(),
-                    text_format_regular,
-                    text_format_bold,
-                    mouse_position: None,
-                },
-            );
+        let text_layout = direct_write_factory.CreateTextLayout(
+            contents,
+            &text_format_regular,
+            window_width,
+            window_height,
+        )?;
 
-            Ok(Box::from_raw(window_ptr))
-        }
+        let mut client_rect = RECT::default();
+        GetClientRect(hwnd, &mut client_rect)?;
+
+        ptr::write(
+            window_ptr,
+            Window {
+                hwnd,
+                render_target,
+                direct_2d_factory,
+                text_brush,
+                hover_brush,
+                width: (client_rect.right - client_rect.left) as u32,
+                height: (client_rect.bottom - client_rect.top) as u32,
+                direct_write_factory,
+                contents: contents_str,
+                contents_utf16,
+                cursor_pos: 0,
+                text_format_regular,
+                text_format_bold,
+                text_layout,
+                mouse_position: None,
+            },
+        );
+
+        Ok(Box::from_raw(window_ptr))
     }
 
-    fn run(&mut self) -> Result<()> {
-        unsafe {
-            _ = ShowWindow(self.hwnd, SW_NORMAL);
+    unsafe fn run(&mut self) -> Result<()> {
+        _ = ShowWindow(self.hwnd, SW_NORMAL);
 
-            let mut message = MSG::default();
+        let mut message = MSG::default();
 
-            while GetMessageW(&mut message, Some(self.hwnd), 0, 0).0 > 0 {
-                _ = TranslateMessage(&message);
-                DispatchMessageW(&message);
-            }
-
-            Ok(())
+        while GetMessageW(&mut message, Some(self.hwnd), 0, 0).0 > 0 {
+            _ = TranslateMessage(&message);
+            DispatchMessageW(&message);
         }
+
+        Ok(())
     }
 
-    unsafe fn paint(&self) -> Result<()> {
+    unsafe fn paint(&mut self) -> Result<()> {
         self.render_target.BeginDraw();
         self.render_target.Clear(Some(&D2D1_COLOR_F {
-            r: 0.3,
-            g: 0.2,
-            b: 0.9,
+            r: 0.0,
+            g: 0.0,
+            b: 0.0,
             a: 1.0,
         }));
 
-        let height = 50f32;
-        let left = 0f32;
-        let right = 500f32;
-        let mut top = 0f32;
-        let mut bottom = height;
-
-        for item in self.file_tree.display_iter() {
-            let text_format = match item.item_type {
-                FileTreeItemType::File => &self.text_format_regular,
-                FileTreeItemType::Directory { .. } => &self.text_format_bold,
-            };
-
-            if let Some(pos) = self.mouse_position {
-                let x = pos.0 as f32;
-                let y = pos.1 as f32;
-
-                if left < x && x < right && top < y && y < bottom {
-                    self.render_target.FillRectangle(&D2D_RECT_F {
-                        left,
-                        right,
-                        bottom,
-                        top,
-                    }, &self.hover_brush);
-                }
-            }
-
-            self.render_target.DrawText(
-                item.name
-                    .as_bytes()
-                    .iter()
-                    .map(|byte| *byte as u16)
-                    .collect::<Box<[u16]>>()
-                    .as_ref(),
-                text_format,
-                &D2D_RECT_F {
-                    left,
-                    right,
-                    bottom,
-                    top,
-                },
-                &self.text_brush,
-                D2D1_DRAW_TEXT_OPTIONS::default(),
-                DWRITE_MEASURING_MODE::default(),
-            );
-
-            top += height;
-            bottom += height;
+        self.render_target.DrawTextLayout(
+            Default::default(),
+            &self.text_layout,
+            &self.text_brush,
+            Default::default(),
+        );
+        let mut x = 0f32;
+        let mut y = 0f32;
+        let mut metrics = DWRITE_HIT_TEST_METRICS::default();
+        self.text_layout.HitTestTextPosition(
+            self.cursor_pos,
+            false,
+            &mut x,
+            &mut y,
+            &mut metrics,
+        )?;
+        const EPSILON: f32 = 0.000001f32;
+        const WIDTH: f32 = 3f32;
+        if x.abs() < EPSILON {
+            x += WIDTH / 2f32;
         }
 
+        self.render_target.FillRectangle(&D2D_RECT_F {
+            left: x - WIDTH / 2f32,
+            right: x + WIDTH / 2f32,
+            top: y,
+            bottom: y + metrics.height,
+        }, &self.text_brush);
+
         self.render_target.EndDraw(None, None)?;
+        Ok(())
+    }
+
+    unsafe fn resize(&mut self, width: u32, height: u32) -> Result<()> {
+        self.render_target
+            .Resize(&D2D_SIZE_U { width, height })
+            .unwrap();
+        _ = InvalidateRect(Some(self.hwnd), None, false);
+
+        self.width = width;
+        self.height = height;
+
+        _ = mem::replace(
+            &mut self.text_layout,
+            self.direct_write_factory.CreateTextLayout(
+                self.contents.as_utf16(&mut self.contents_utf16),
+                &self.text_format_regular,
+                width as f32,
+                height as f32,
+            )?,
+        );
+
         Ok(())
     }
 
@@ -221,12 +265,14 @@ impl Window {
         lparam: LPARAM,
     ) -> LRESULT {
         if message == WM_CREATE {
-            let create_struct: *const CREATESTRUCTW = mem::transmute(lparam.0);
-            let create_struct = create_struct.as_ref().expect("lParam ptr for WM_CREATE should not be null");
+            let create_struct = lparam.0 as *const CREATESTRUCTW;
+            let create_struct = create_struct
+                .as_ref()
+                .expect("lParam ptr for WM_CREATE should not be null");
             let window_ptr = create_struct.lpCreateParams as *mut Window;
             SetWindowLongPtrW(hwnd, GWLP_USERDATA, mem::transmute(window_ptr));
 
-            return LRESULT(0)
+            return LRESULT(0);
         }
 
         let window_ptr: *mut Window = mem::transmute(GetWindowLongPtrW(hwnd, GWLP_USERDATA));
@@ -245,13 +291,7 @@ impl Window {
             WM_SIZE => {
                 let width: u32 = (lparam.0 & 0xffff) as u32;
                 let height: u32 = ((lparam.0 & 0xffff0000) >> 16) as u32;
-                window
-                    .render_target
-                    .Resize(&D2D_SIZE_U { width, height })
-                    .unwrap();
-                _ = InvalidateRect(Some(hwnd), None, false);
-                window.width = width;
-                window.height = height;
+                window.resize(width, height);
                 LRESULT(0)
             }
             WM_PAINT => {
@@ -268,7 +308,19 @@ impl Window {
                 _ = InvalidateRect(Some(hwnd), None, false);
 
                 LRESULT(0)
-            },
+            }
+            WM_KEYDOWN => {
+                let mut handled = true;
+                match VIRTUAL_KEY(wparam.0 as u16) {
+                    VK_LEFT => window.cursor_pos = window.cursor_pos.saturating_sub(1),
+                    VK_RIGHT => window.cursor_pos += 1,
+                    _ => handled = false,
+                };
+                if handled {
+                    _ = InvalidateRect(Some(hwnd), None, false);
+                }
+                LRESULT(0)
+            }
             WM_QUIT | WM_DESTROY => {
                 PostQuitMessage(0);
                 LRESULT(0)
@@ -279,8 +331,8 @@ impl Window {
 }
 
 fn main() -> Result<()> {
-    let mut window = Window::new(w!("Direct2D Rust Window"))?;
-    window.run()?;
+    let mut window = unsafe { Window::new(w!("Direct2D Rust Window"))? };
+    unsafe { window.run()? };
 
     Ok(())
 }
